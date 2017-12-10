8a06d4878d8959942add3c22e3dd9438fe8f5f21
ZichaoX <34415367+ZichaoX@users.noreply.github.com>
2017-12-10 19:56:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Generate random room name if needed
+// 创建一个聊天房间
 if (!location.hash) {
   location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 const roomHash = location.hash.substring(1);
 
-// TODO: Replace with your own channel ID
-const drone = new ScaleDrone('63o6Zfoz6yeAcJDG');
-// Room name needs to be prefixed with 'observable-'
+// 替换为你的信道ID
+const drone = new ScaleDrone('67pBNBMXmm3io3hw');
+// 房间名前缀为'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
   iceServers: [{
@@ -34,17 +34,16 @@ drone.on('open', error => {
       onError(error);
     }
   });
-  // We're connected to the room and received an array of 'members'
-  // connected to the room (including us). Signaling server is ready.
+  // 连接房间成功并且在数组中记录成员
+  // 信令服务器准备就绪
   room.on('members', members => {
     console.log('MEMBERS', members);
-    // If we are the second user to connect to the room we will be creating the offer
     const isOfferer = members.length === 2;
     startWebRTC(isOfferer);
   });
 });
 
-// Send signaling data via Scaledrone
+// 通过Scaledrone传输信令
 function sendMessage(message) {
   drone.publish({
     room: roomName,
@@ -54,23 +53,19 @@ function sendMessage(message) {
 
 function startWebRTC(isOfferer) {
   pc = new RTCPeerConnection(configuration);
-
-  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
-  // message to the other peer through the signaling server
   pc.onicecandidate = event => {
     if (event.candidate) {
       sendMessage({'candidate': event.candidate});
     }
   };
 
-  // If user is offerer let the 'negotiationneeded' event create the offer
+  // 发起者通过'negotiationneeded'建立连接
   if (isOfferer) {
     pc.onnegotiationneeded = () => {
       pc.createOffer().then(localDescCreated).catch(onError);
     }
   }
 
-  // When a remote stream arrives display it in the #remoteVideo element
   pc.onaddstream = event => {
     remoteVideo.srcObject = event.stream;
   };
@@ -79,29 +74,25 @@ function startWebRTC(isOfferer) {
     audio: true,
     video: true,
   }).then(stream => {
-    // Display your local video in #localVideo element
+    // 打开你的摄像设备
     localVideo.srcObject = stream;
-    // Add your stream to be sent to the conneting peer
+    // 发送你的视频流
     pc.addStream(stream);
   }, onError);
 
-  // Listen to signaling data from Scaledrone
+  // 从Scaledrone监听
   room.on('data', (message, client) => {
-    // Message was sent by us
     if (client.id === drone.clientId) {
       return;
     }
 
     if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
       pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        // When receiving an offer lets answer it
         if (pc.remoteDescription.type === 'offer') {
           pc.createAnswer().then(localDescCreated).catch(onError);
         }
       }, onError);
     } else if (message.candidate) {
-      // Add the new ICE candidate to our connections remote description
       pc.addIceCandidate(
         new RTCIceCandidate(message.candidate), onSuccess, onError
       );
